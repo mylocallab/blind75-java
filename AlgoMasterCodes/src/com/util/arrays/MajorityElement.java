f23947e144fbc47b1cbc9eb47b8a63f8ffbcd99c
shiva <d.shiva0212@gmail.com>
2026-05-20 20:51:19 +0530

    This is commit frmo UI

--- AlgoMasterCodes/src/com/util/arrays/MajorityElement.java
+++ AlgoMasterCodes/src/com/util/arrays/MajorityElement.java
@@ -4,7 +4,7 @@ public class MajorityElement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("New ");
+        System.out.println("New commit frmo UI");
 	}
 
 }
